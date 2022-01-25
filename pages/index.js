e2268aa652ef61ed4d9e789aacf04a6b46828ba2
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2022-01-25 10:58:17 -0300

    Compponent Layout and Title

--- pages/index.js
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="bg-gradient-to-r from-blue-500 to-red-800 flex justify-center items-center text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
-  );
-}
